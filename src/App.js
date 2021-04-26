6a21200634355baca1d9c3e33471522d2a203fa7
ucesur <ucesur@gmail.com>
2021-04-26 23:43:38 +0300

    Learning FirebaseDatabaseProvider and FirebaseDatabaseNode usage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import SearchPage from './components/SearchPage.js';
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-
-import {config} from "./config.js"
 
 function App() {
-  // Initialize Firebase
-  if (!firebase.apps.length) {
-     firebase.initializeApp(config);
-  }else {
-     firebase.app(); // if already initialized, use that one
-  }
   return (
     <div className="App">
       <SearchPage/>
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from "firebase/app";
+import "firebase/database";
+import {
+  FirebaseDatabaseProvider,
+  FirebaseDatabaseNode
+} from "@react-firebase/database";
+import { config } from "../config";
 
 const RecipeCard = props => {
+  const [limit, setLimit] = useState(1);
+  const s = (a: any) => JSON.stringify(a, null, 2);
   return (
-    <div className="card text-center">
-      <div className="overflow">
-        Image Area
-      </div>
-      <div className="card-body">
-        <h4 className="card-title">Title</h4>
-        <p className="card-text text-secondary">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-        <div className="">
-          <p>tags</p>
-        </div>
-      </div>
+    <div>
+      <FirebaseDatabaseProvider firebase={firebase} {...config}>
+        <FirebaseDatabaseNode
+          path="/"
+          limitToFirst={limit}
+          orderByKey
+          // orderByValue={"created_on"}
+        >
+          {d => {
+              return(
+                <React.Fragment>
+                  <pre>
+                    <div className="card text-center">
+                      <div className="overflow">
+                        Image Area
+                      </div>
+                      <div className="card-body">
+                        <h4 className="card-title">{s(d.value)}</h4>
+                        <p className="card-text text-secondary">
+                          Lorem Ipsum is simply dummy text of the printing and typesetting
+                          industry.Lorem Ipsum has been the industry's standard dummy text ever
+                          since the 1500s, when an unknown printer took a galley of type and
+                          scrambled it to make a type specimen book.
+                        </p>
+                        <div className="ddd">
+                          <p>tags</p>
+                        </div>
+                      </div>
+                    </div>
+                  </pre>
+                </React.Fragment>
+              );
+            }
+          }
+        </FirebaseDatabaseNode>
+      </FirebaseDatabaseProvider>
     </div>
   );
 }
