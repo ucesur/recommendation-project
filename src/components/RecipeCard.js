794860caa64481311d1523fc59e1650facf39b0c
ucesur <ucesur@gmail.com>
2021-05-02 12:46:07 +0300

    Working on database learning continues. Used collection of firebase.

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,52 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import firebase from "firebase/app";
-import "firebase/database";
-import {
-  FirebaseDatabaseProvider,
-  FirebaseDatabaseNode
-} from "@react-firebase/database";
-import { config } from "../config";
+
+import db from "../config";
 
 const RecipeCard = props => {
-  const [limit, setLimit] = useState(1);
-  const s = (a: any) => JSON.stringify(a, null, 2);
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+
+  useEffect(() => {
+    db.collection("recipes").onSnapshot(snapshot => {
+      setTitle(snapshot.docs.map(doc => doc.data().title));
+      setDescription(snapshot.docs.map(doc => doc.data().description));
+    })
+  },[]);
+
   return (
-    <div>
-      <FirebaseDatabaseProvider firebase={firebase} {...config}>
-        <FirebaseDatabaseNode
-          path="/"
-          limitToFirst={limit}
-          orderByKey
-          // orderByValue={"created_on"}
-        >
-          {d => {
-              return(
-                <React.Fragment>
-                  <pre>
-                    <div className="card text-center">
-                      <div className="overflow">
-                        Image Area
-                      </div>
-                      <div className="card-body">
-                        <h4 className="card-title">{s(d.value)}</h4>
-                        <p className="card-text text-secondary">
-                          Lorem Ipsum is simply dummy text of the printing and typesetting
-                          industry.Lorem Ipsum has been the industry's standard dummy text ever
-                          since the 1500s, when an unknown printer took a galley of type and
-                          scrambled it to make a type specimen book.
-                        </p>
-                        <div className="ddd">
-                          <p>tags</p>
-                        </div>
-                      </div>
-                    </div>
-                  </pre>
-                </React.Fragment>
-              );
-            }
-          }
-        </FirebaseDatabaseNode>
-      </FirebaseDatabaseProvider>
+    <div className="card text-center">
+      <div className="overflow">
+        Image Area
+      </div>
+      <div className="card-body">
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text text-secondary">{description}</p>
+        <div className="ddd">
+          <p>tags</p>
+        </div>
+      </div>
     </div>
   );
 }
